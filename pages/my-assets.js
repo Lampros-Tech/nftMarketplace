8790e0322915b3158d2dc8bfc35777c71c2e4ced
RahulAtLamp <rahulrajan@lampros.tech>
2022-01-07 19:30:36 +0530

    07-01-22 Current Work

---
+++ pages/my-assets.js
@@ -0,0 +1,195 @@
+import { ethers } from 'ethers'
+import { useEffect, useState, Fragment, useRef } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import axios from 'axios'
+import Web3Modal from "web3modal"
+
+import {
+  nftmarketaddress, nftaddress
+} from '../config'
+
+import Market from '../artifacts/contracts/Market.sol/NFTMarket.json'
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+
+export default function MyAssets() {
+  const [nfts, setNfts] = useState([])
+  const [loadingState, setLoadingState] = useState('not-loaded')
+  
+  const [open, setOpen] = useState(false)
+  const cancelButtonRef = useRef(null)
+
+  var newPrice = null
+    
+  // var sellTokenId = null
+  
+  useEffect(() => {
+    loadNFTs()
+  }, [])
+  async function loadNFTs() {
+    const web3Modal = new Web3Modal({
+      network: "mainnet",
+      cacheProvider: true,
+    })
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+      
+    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
+    const data = await marketContract.fetchMyNFTs()
+    
+    const items = await Promise.all(data.map(async i => {
+      const tokenUri = await tokenContract.tokenURI(i.tokenId)
+      const meta = await axios.get(tokenUri)
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+      let item = {
+        price,
+        markeId: i.nftContract,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+      }
+      return item
+    }))
+
+
+    setNfts(items)
+    setLoadingState('loaded') 
+  }
+
+  async function sellNft() {
+    console.log(nftmarketaddress, nftaddress, window.sellTokenId, newPrice)
+
+    if(newPrice === null || newPrice === undefined || newPrice === ""){
+
+    }else{
+      const web3Modal = new Web3Modal()
+      const connection = await web3Modal.connect()
+      const provider = new ethers.providers.Web3Provider(connection)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+      let listingPrice = await contract.getListingPrice()
+      listingPrice = listingPrice.toString()
+
+      // console.log(typeof(nftaddress))
+
+      //const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')
+      const price = ethers.utils.parseUnits(newPrice, 'ether')
+
+      setOpen(false)
+
+      const transaction = await contract.putItemToResell( nftaddress, window.sellTokenId, price,{ value: listingPrice } )
+      await transaction.wait()
+
+      loadNFTs()
+    }
+  }
+
+  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No assets owned</h1>)
+  return (
+    <div className="flex justify-center">
+      <div className="p-4 main-container">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {
+            nfts.map((nft, i) => (
+              <div key={i} className="border shadow rounded-xl overflow-hidden">
+                <div className='imageholder'><img src={nft.image} className="rounded"/></div>
+                <div className="p-4 bg-black">
+                  <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
+                  <button className="w-full text-white font-bold py-2 px-12 mt-4 rounded bs-btn" onClick={ () => { setOpen(true); window.sellTokenId = nft.tokenId; } }>Sell</button>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+        <Transition.Root show={open} as={Fragment}>
+          <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={cancelButtonRef} onClose={setOpen}>
+            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+              </Transition.Child>
+
+              {/* This element is to trick the browser into centering the modal contents. */}
+              <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+                &#8203;
+              </span>
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="sm:block sm:items-start">
+                      {/* <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                      </div> */}
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                          Resell
+                        </Dialog.Title>
+                        <div className="mt-2">
+                          <div className="text-sm text-gray-500">
+                            <input 
+                              className="shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                              style={{maxWidth:'500px', width:'100%'}} 
+                              type="number" 
+                              placeholder="Please enter the resale value..." 
+                              onChange={(event)=>{
+                                newPrice = event.target.value
+                              }}
+                              required
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                    <button
+                      type="button"
+                      className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-md"
+                      onClick={() => sellNft()}
+                    >
+                      Sell
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                      onClick={() => setOpen(false)}
+                      ref={cancelButtonRef}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition.Root>
+        {/* <div className="float-right w-64 sticky right-0">
+          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded absolute" role="alert">
+            <strong className="font-bold">Holy smokes!</strong>
+            <span className="block sm:inline">Something seriously bad happened.</span>
+            <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+              <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+            </span>
+          </div>
+        </div> */}
+      </div>
+    </div>
+  )
+}
